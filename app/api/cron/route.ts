fb53cec204f88fcc5b2e4f9d75963290eca3c4ad
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2023-12-10 22:19:37 +0530

    Build Check and CRON updated

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,10 @@ import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } f
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 
 export async function GET() {
     try {
