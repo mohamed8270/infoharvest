dd4f01b9df39362424d8ec4198e983713b73081f
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2023-12-11 14:27:52 +0530

    CRON update & Function capability, Performance insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '../components/NavBar'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavBar/>
         {children}
+        <SpeedInsights />
         </body>
     </html>
   )
