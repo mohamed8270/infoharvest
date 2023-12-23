7cae72cc423ab5e93fe04d0126b6e84d86448a28
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2023-12-23 17:12:39 +0530

    Similar Products limit updated

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -83,7 +83,7 @@ export async function getSimilarProducts(productId: string) {
 
         const similarProducts = await Product.find({
             _id: {$ne: productId},
-        }).limit(3);
+        }).limit(10);
 
         return similarProducts;
     } catch (error) {
