39a555487a399dc40172aaccc5cb57de07c28eb7
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-01-28 21:13:00 +0530

    Google Adsense Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import Image from 'next/image'
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
     <div className='bg-white'>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6678637984319183"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <HeroSection/>
     </div>
   )
