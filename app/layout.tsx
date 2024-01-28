b51b8136e880571ecabcfe7a8eb0a814d1f673be
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-01-28 21:25:38 +0530

    Meta Data update for Google Adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '../components/NavBar'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +19,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <>
+    <Head>
+      <meta name="google-adsense-account" content="ca-pub-6678637984319183" />
+    </Head>
     <html lang="en">
       <body className={inter.className}>
         <NavBar/>
         {children}
         <SpeedInsights />
+        <Analytics />
         </body>
     </html>
+    </>
   )
 }
